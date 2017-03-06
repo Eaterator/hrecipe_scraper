4b8b9b32c39243e1e733cb1eebaa5a4fa5ebf807
lucas <currahl@yahoo.ca>
2017-03-06 13:15:12 -0500

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name="Recipe Scraper",
     version='0.1',
+    description="Simple scraper for hrecipe formats. Runs async in one thread and writes to a text file.",
+    url="https://github.com/Eaterator/hrecipe_scraper",
+    author="Lucas Currah",
+    license="MIT",
+    keywords="hrecipe parser scraper recipe cooking",
+    packages=find_packages(exclude=['tests*'])
 )
